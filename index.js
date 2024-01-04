ea41e891a64d914882f2d3f14e6fad886402585f
kelvinkuria <kkuria383@yahoo.com>
2024-01-04 11:23:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// index.js
+
+// Define the addingEventListener function
 function addingEventListener() {
-}
+    // Grab the element that you want to add the event listener to
+    const input = document.getElementById('input');
+  
+    // Create a named function for the callback
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the click event listener to the element
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Export the addingEventListener function if you need to test it
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = addingEventListener;
+  }
+  
+
